0ec325b8f402f473d4d40443c835b9f90951f62c
João Henrique Zanette <146747026+Zanette93@users.noreply.github.com>
2024-06-27 16:07:34 -0300

    Update app.py

--- LogBook/app.py
+++ LogBook/app.py
@@ -10,7 +10,7 @@ from itertools import zip_longest
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "minhachavesecreta"
-app.config["MAIL_SERVER"] = "smtp-relay.brevo.com"
+app.config["MAIL_SERVER"] = "smtpmailhere"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "youremailhere" 
